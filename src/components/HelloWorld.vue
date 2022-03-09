f8eef07827dcf0b8ab7500fc4dd5a8761742d201
javaliyuanyuan <javaliyuanyuan@163.com>
2022-03-09 18:21:53 +0800

    测试HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,18 @@
 <template>
-<span class="title">hello world</span>
+    <span class="title">hello world</span>
+    <button @click="count += 1">点击</button>
+    <span>点击次数:{{ count }}</span>
 </template>
+<script lang="ts" setup>
+import { ref } from "vue"
+
+const count = ref(0)
+</script>
 <style lang="scss" scoped>
-.title{
+.title {
     font-size: 42px;
+}
+span {
+    display: block;
 }
 </style>
---
+++ test/HelloWorld.spec.ts
@@ -0,0 +1,9 @@
+import HelloWorld from "@/components/HelloWorld.vue"
+import { shallowMount } from "@vue/test-utils"
+describe("组件HelloWorld", () => {
+    test("是否包含button", () => {
+        const wrapper = shallowMount(HelloWorld)
+        const element = wrapper.find("div > #app > button")
+        expect(element.exists()).toBeTruthy()
+    })
+})
