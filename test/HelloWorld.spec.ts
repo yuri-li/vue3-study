a29158d1df450e0545575ec570ecef87006ff8fc
javaliyuanyuan <javaliyuanyuan@163.com>
2022-03-10 11:19:13 +0800

    测试HelloWorld.vue

--- test/HelloWorld.spec.ts
+++ test/HelloWorld.spec.ts
@@ -6,4 +6,16 @@ describe("组件HelloWorld", () => {
         const element = wrapper.find("#div-btn > button")
         expect(element.exists()).toBeTruthy()
     })
+    test("获取count的默认值",()=>{
+        const wrapper = shallowMount(HelloWorld)
+        expect(wrapper.vm.count).toBe(0)
+    })
+    test("触发button 点击事件，观察点击前后count的值", async ()=>{
+        const wrapper = shallowMount(HelloWorld)
+        expect(wrapper.vm.count).toBe(0)
+
+        const element = wrapper.find("#div-btn > button")
+        await element.trigger("click")
+        expect(wrapper.vm.count).toBe(1)
+    })
 })
